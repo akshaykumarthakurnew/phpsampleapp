f543968a4c114ba1c135f1ac7826f4d3385f243f
akshaykumarthakurnew <109959999+akshaykumarthakurnew@users.noreply.github.com>
2024-06-15 10:07:29 +0200

    Create diagram.py

---
+++ diagram.py
@@ -0,0 +1,29 @@
+# diagram.py
+
+from diagrams import Diagram, Cluster
+from diagrams.azure.compute import VM
+from diagrams.azure.network import VNet, Subnet, NIC
+from diagrams.azure.database import SQLDatabase
+from diagrams.azure.identity import ActiveDirectory
+from diagrams.azure.storage import StorageAccounts
+
+with Diagram("Azure Infrastructure", show=False):
+    with Cluster("Resource Group: example-resources"):
+        with Cluster("VNet: example-network"):
+            subnet = Subnet("example-subnet")
+
+            vm1 = VM("example-vm")
+            new_vm = VM("new-vm")
+
+            subnet - [vm1, new_vm]
+        
+        sql_db = SQLDatabase("example-db")
+        ad = ActiveDirectory("example-ad")
+
+        storage = StorageAccounts("examplestorageacct")
+
+    subnet >> sql_db
+    vm1 >> ad
+    new_vm >> ad
+    vm1 >> storage
+    new_vm >> storage
